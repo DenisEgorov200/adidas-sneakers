6124c0e767b6e15713006a2b5557986aef2e4f9d
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-04-05 22:15:14 +0300

    feat: add spinner in loading page

--- src/shared/ui/loading/index.tsx
+++ src/shared/ui/loading/index.tsx
@@ -1,7 +1,28 @@
 import { useProgress } from '@react-three/drei'
+import clsx from 'clsx'
+import { useEffect } from 'react'
 
 export const Loading = () => {
   const { progress } = useProgress()
 
-  return <div className="text-3xl">{progress}</div>
+  useEffect(() => {
+    document.body.classList.toggle('overflow-hidden', progress < 100)
+  }, [progress])
+
+  return (
+    <>
+      {progress < 100 && (
+        <div
+          className={clsx(
+            'fixed z-50 w-full h-full flex flex-col items-center justify-center text-3xl select-none overflow-hidden',
+            {
+              'bg-gray-200': progress < 100,
+            },
+          )}>
+          <img src="/icons/loading.svg" alt="loading" />
+          <h1>Loading...</h1>
+        </div>
+      )}
+    </>
+  )
 }
